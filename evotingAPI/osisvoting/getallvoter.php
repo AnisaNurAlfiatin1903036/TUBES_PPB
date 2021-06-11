a00b925e07a08f041c05fc93cf7a451f157bf410
Anisa Nur Alfiatin <81427689+AnisaNurAlfiatin1903036@users.noreply.github.com>
2021-06-11 08:54:16 +0700

    Add files via upload

---
+++ evotingAPI/osisvoting/getallvoter.php
@@ -0,0 +1,18 @@
+<?php
+include 'db.php';
+
+$sql = "SELECT COUNT(*) FROM voters";
+
+$result = array();
+
+$query = $conn->query($sql);
+
+
+while ($row = $query->fetch_assoc()) {
+    $result[] = $row;
+}
+if ($query->num_rows > 0) {
+    echo json_encode(['data' => $result]);
+} else {
+    echo json_encode(['error' => 'Kata Sandi Salah!!']);
+}
---
+++ evotingAPI/osisvoting/getvoteketua.php
@@ -0,0 +1,20 @@
+
+<?php
+include 'db.php';
+
+$sql = "SELECT votes.*, candidates.id, candidates.firstname, candidates.lastname, COUNT(*) FROM votes INNER JOIN candidates ON votes.candidate_id = candidates.id where votes.position_id = 1 GROUP BY votes.candidate_id
+";
+
+$result = array();
+
+$query = $conn->query($sql);
+
+
+while ($row = $query->fetch_assoc()) {
+    $result[] = $row;
+}
+if ($query->num_rows > 0) {
+    echo json_encode(['data' => $result]);
+} else {
+    echo json_encode(['error' => 'Kata Sandi Salah!!']);
+}
